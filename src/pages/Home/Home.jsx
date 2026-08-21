c5f5b9e35d50641c5434000dcc2e8d1c43447ecb
MAHATAB100 <mahatabhossan78@gmail.com>
2026-08-21 19:25:25 +0600

    Jobs data load

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Bannner from './Bannner';
+import HotJobs from './HotJobs';
 
 const Home = () => {
     return (
         <div>
             <Bannner></Bannner>
+            <HotJobs></HotJobs>
         </div>
     );
 };
